71d4fb95670674ccdb14f28daf33f9d2d42a9cc3
Matheussmaced <mateusbyakuya3@gmail.com>
2024-04-29 11:41:32 -0300

    feat: variaveis

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        int ano = 2021;
+        ano = 2022;
+
+        final String BR = "Brasil"; // final significa que a variavel nunca será alterada
+
+        System.err.println(ano);
     }
 }
