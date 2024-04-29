6d1d96664703431521a50baeeebb27edeaeb7a52
Matheussmaced <mateusbyakuya3@gmail.com>
2024-04-29 11:52:40 -0300

    feat: chamada de metodo (mais de um public static na classe principal e chamando ela na classe principal)

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -1,12 +1,24 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
 
         int ano = 2021;
         ano = 2022;
 
         final String BR = "Brasil"; // final significa que a variavel nunca será alterada
 
-        System.err.println(ano);
+        // System.err.println(ano);
+
+        String primeiroNome = "Matheus";
+        String segundoNome = "Gregorio";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome); // chamada de um metodo
+
+        System.err.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do método: " + primeiroNome.concat((" ").concat(segundoNome));
     }
 }
