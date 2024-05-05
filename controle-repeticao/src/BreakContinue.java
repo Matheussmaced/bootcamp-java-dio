78bdaf99bf450c27275db84f0c2c0bc1246c6897
Matheussmaced <mateusbyakuya3@gmail.com>
2024-05-05 15:01:32 -0300

    feat: repetição

---
+++ controle-repeticao/src/BreakContinue.java
@@ -0,0 +1,13 @@
+public class BreakContinue {
+  public static void main(String[] args) {
+
+    for (int numero = 1; numero <= 5; numero++) {
+      if (numero == 3)
+        break; // continue ele continua o codigo
+
+      System.out.println(numero);
+
+    }
+
+  }
+}
