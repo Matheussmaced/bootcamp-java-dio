cc1df13ff6de6c07380aaaf20b313e90c28afc7e
Matheussmaced <mateusbyakuya3@gmail.com>
2024-05-13 20:06:15 -0300

    feat: aplicando encapsulamento na pratica

---
+++ MSNMessenger/src/ComputadorDoPedrinho.java
@@ -0,0 +1,9 @@
+public class ComputadorDoPedrinho {
+
+  public static void main(String[] args) {
+    MSNMessenger msnMessenger = new MSNMessenger();
+
+    msnMessenger.enviarMensagem();
+    msnMessenger.receberMensagem();
+  }
+}
---
+++ MSNMessenger/src/MSNMessenger.java
@@ -0,0 +1,19 @@
+public class MSNMessenger {
+    public void enviarMensagem() {
+        validarConectadoInternet();
+        System.out.println("Enviando mensagem");
+        salvarHistoricoMensagem();
+    }
+
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem");
+    }
+
+    private void validarConectadoInternet() {
+        System.out.println("Verificando se há rede");
+    }
+
+    private void salvarHistoricoMensagem() {
+        System.out.println("Salvando historico de mensagem");
+    }
+}
